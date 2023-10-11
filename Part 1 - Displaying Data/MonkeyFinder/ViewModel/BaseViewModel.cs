fa4d0207b63c521717d57608cf9652626f8c2828
MPIEKARCZYK <mpiekarczyk@senga.com.pl>
2023-10-11 16:09:28 +0200

    1.53

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,18 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public class BaseViewModel
+public partial class BaseViewModel : ObservableObject
 {
+    public BaseViewModel()
+    {
+
+    }
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsnotBusy))]
+    public bool isBusy;
+
+    [ObservableProperty]
+    public string title;
+
+    public bool IsnotBusy => !IsBusy;
 }
